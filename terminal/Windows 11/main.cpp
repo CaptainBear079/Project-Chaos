bb669578ce874ce2ab7f3e9d9a53db83ece20ecc
Louis Ritz <louis.ritz12@gmail.com>
2025-04-28 14:42:35 +0200

    terminal command controll added

--- terminal/Windows 11/main.cpp
+++ terminal/Windows 11/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 
-int terminal() {}
+int check_command(char* argv) {
+	if (argv == "") {
 
-int main() {
-	if(terminal() != 0) {
+	}
+	else if (argv == "-disable_info") {
+		return 1;
+	}
+	else if (argv == "-run_as_admin") {
+		return 2;
+	}
+	return -1;
+}
+
+int terminal(char* argv[]) {
+	for (int i = 0;i < 3;i++) {
+		check_command(argv[i]);
+	}
+	std::cout << "Type in a command: $/"
+	std::cin >> input;
+}
+
+int main(int argc, char* argv[]) {
+	if(terminal(argv[1]) != 0) {
 		return -1;
 	}
+	return 0;
 }
