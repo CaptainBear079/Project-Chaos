6daed1b660ee1fb6176d2745532c0b2b7aafee8e
Louis Ritz <louis.ritz12@gmail.com>
2025-05-03 12:58:26 +0200

    BOOT.asm updated and DataManager Added

--- bin/BOOT.asm
+++ bin/BOOT.asm
@@ -3,6 +3,28 @@ bits 16
 
 %define ENDL 0x0D, 0x0A
 
+; FAT 12 Headers
+jmp short _start
+nop
+
+bdb_oem:                   db 'MSWIN4.1'
+bdb_bytes_per_sector:      dw 512
+bdb_sectors_per_cluster:   db 1
+bdb_reserved_srctors:      dw 1
+bdb_fat_count:             db 2
+bdb_dir_entries_count:     dw 0xE0
+bdb_total_sectors:         dw 2880
+bdb_media_descriptor_type: db 0xF0
+bdb_sectors_per_fat:       dw 9
+bdb_sectors_per_track:     dw 18
+bdb_heads:                 dw 2
+bdb_hidden_sectors:        dd 0
+bdb_large_sector_count:    dd 0
+
+ebr_drive_number:          db 0
+                           db 0
+ebr_signature:
+
 _start:
 jmp main
 
