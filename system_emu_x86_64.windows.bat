6a36edf87aebd4e6175e6dc0630f7f4e491991e3
Louis Ritz <louis.ritz12@gmail.com>
2025-07-08 17:46:37 +0200

    Compile and Run in Qemu Batch

---
+++ system_emu_x86_64.windows.bat
@@ -0,0 +1,9 @@
+TITLE System Emulator x86_64
+REM This script builds the FAT32 boot image and runs it in QEMU.
+START /wait nasm ^
+  -f bin ^
+  -o ./build/boot.bin ^
+  ./bin/BOOT.asm
+REM Create the boot image with a FAT32 filesystem
+START qemu-system-x86_64 ^
+  ./build/boot.bin
